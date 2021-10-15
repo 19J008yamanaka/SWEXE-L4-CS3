2a93a2673b6ca70e0c80e89e3282fa42f7de5fa5
yamanaka <1911140008j@ed.fuk.kindai.fuk.ac.jp>
2021-10-15 08:27:18 +0000

    課題３

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'top/main'
+  post 'top/login'
+  root 'top#main'
+  get 'top/logout'
+  resources :top
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
